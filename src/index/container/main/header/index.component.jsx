730781de708c56d8681694f758ef8c4b6d1353cb
yycsc325 <m13625744973@163.com>
2019-10-31 22:17:34 +0800

    modify

--- src/index/container/main/header/index.component.jsx
+++ src/index/container/main/header/index.component.jsx
@@ -12,11 +12,11 @@ class Index extends Component {
         collapsed: false,
       };
     
-      toggle = () => {
-        this.setState({
-          collapsed: !this.state.collapsed,
-        });
-      };
+    toggle = () => {
+      this.setState({
+        collapsed: !this.state.collapsed,
+      });
+    };
     componentWillMount() {
 
     }
@@ -31,56 +31,7 @@ class Index extends Component {
 
     render() {
         return (
-            // <div className='header'>
-            //     <div className='title'>
-            //         React-Demo
-            //     </div>
-            //     <Button>按钮</Button>
-            //     <DatePicker></DatePicker>
-            //     <div className='header-menu'>
-            //         {
-            //             '嘻嘻哈哈'
-            //         }
-            //     </div>
-            // </div>
-      <Layout style={{height:"100vh"}}>
-        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-          <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">
-              <Icon type="user" />
-              <span>nav 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="video-camera" />
-              <span>nav 2</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="upload" />
-              <span>nav 3</span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
-        <Layout>
-          <Header style={{ background: '#fff', padding: 0 }}>
-            <Icon
-              className="trigger"
-              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-              onClick={this.toggle}
-            />
-          </Header>
-          <Content
-            style={{
-              margin: '24px 16px',
-              padding: 24,
-              background: '#fff',
-              minHeight: 280,
-            }}
-          >
-            Content
-          </Content>
-        </Layout>
-      </Layout>
+          <div></div>
         )
     }
 }
