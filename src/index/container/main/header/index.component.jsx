ceeef15651237db255806e0c8fc2e95f1c6d7cad
wb-csc612502@antfin.com <wb-csc612502@antfin.com>
2019-10-31 17:43:01 +0800

    allreadey

--- src/index/container/main/header/index.component.jsx
+++ src/index/container/main/header/index.component.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import './index.less';
-import { Button,DatePicker } from 'antd'
+import { Button,DatePicker,Layout, Menu, Icon } from 'antd'
 
+const { Header, Sider, Content } = Layout;
 class Index extends Component {
     constructor(props) {
         super(props);
         this.state={}
     }
-
+    state = {
+        collapsed: false,
+      };
+    
+      toggle = () => {
+        this.setState({
+          collapsed: !this.state.collapsed,
+        });
+      };
     componentWillMount() {
 
     }
@@ -22,18 +31,56 @@ class Index extends Component {
 
     render() {
         return (
-            <div className='header'>
-                <div className='title'>
-                    React-Demo
-                </div>
-                {/* <Button>按钮</Button>
-                <DatePicker></DatePicker> */}
-                <div className='header-menu'>
-                    {
-                        '嘻嘻哈哈'
-                    }
-                </div>
-            </div>
+            // <div className='header'>
+            //     <div className='title'>
+            //         React-Demo
+            //     </div>
+            //     <Button>按钮</Button>
+            //     <DatePicker></DatePicker>
+            //     <div className='header-menu'>
+            //         {
+            //             '嘻嘻哈哈'
+            //         }
+            //     </div>
+            // </div>
+            <Layout>
+        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Icon type="user" />
+              <span>nav 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="video-camera" />
+              <span>nav 2</span>
+            </Menu.Item>
+            <Menu.Item key="3">
+              <Icon type="upload" />
+              <span>nav 3</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className="trigger"
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+          </Header>
+          <Content
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              background: '#fff',
+              minHeight: 280,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
         )
     }
 }
